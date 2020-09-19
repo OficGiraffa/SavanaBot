c73ab4802266e759cbc8a43b15ab07a89d5baf1f
Glitch (hello-express) <none>
2020-09-19 17:38:57 +0000

    🛳🐩 Checkpoint ./commands/say.js:22651380/100 ./watch.json:22651380/6

--- commands/say.js
+++ commands/say.js
@@ -3,18 +3,17 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
   const sayMessage = args.join(" ");
   
-  let channelToSend = message.mentions.channels;
-  console.log (channelToSend);
-  
+  let channelToSend = message.mentions.channels.first();
   
   if (sayMessage !== ""){
     message.delete().catch(() => {});
     
     let embed = new Discord.MessageEmbed()
-    .setDescription(sayMessage)
+    .setDescription(sayMessage.replace(channelToSend, ""))
     .setColor("RANDOM")
     .setFooter("Por: " + message.author.username);
     
-    channelToSend.send("hello");
+    
+    channelToSend.send(embed);
   }
 }
