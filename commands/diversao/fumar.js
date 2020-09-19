253a324cc133c97c34119705a747cc348a4ec836
Glitch (hello-express) <none>
2020-09-19 22:10:10 +0000

    🎖📕 Checkpoint ./commands/diversao/fumar.js:22651380/536 ./watch.json:22651380/23

--- commands/diversao/fumar.js
+++ commands/diversao/fumar.js
@@ -1,12 +1,9 @@
 module.exports.run = async (client, message, args) => {
-  let msg = message.channel.send("🚬").then(async (msg) => {
-    await client.setTimeout(() => {msg.edit("🚬  💨")}, 1000);
-    await client.setTimeout(() => {msg.edit("🚬      💨")}, 1000);
-    await client.setTimeout(() => {msg.edit("🚬 💨        💨")}, 1000);
-    await client.setTimeout(() => {msg.edit("🚬      💨       💨")}, 1000);
-    await client.setTimeout(() => {msg.edit("🚬           💨")}, 1000);
-    await client.setTimeout(() => {msg.edit("🚬                    💨")}, 1000);
-    await client.setTimeout(() => {msg.edit("🚬                      💨 ")}, 1000);
-    msg.edit("🚬");
-    })
+  let msg = message.channel.send("🚬");
+  client.setTimeout((msg) => {msg.edit("🚬💨")}, 1000);
+  client.setTimeout((msg) => {msg.edit("🚬  💨")}, 1000);
+  client.setTimeout((msg) => {msg.edit("🚬💨  💨")}, 1000);
+  client.setTimeout((msg) => {msg.edit("🚬  💨")}, 1000);
+  client.setTimeout((msg) => {msg.edit("🚬    💨")}, 1000);
+  client.setTimeout((msg) => {msg.edit("🚬")}, 0);
 }
