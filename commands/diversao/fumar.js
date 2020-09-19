30456d223b240dffda7bc9eb29eb65e30791b026
Glitch (hello-express) <none>
2020-09-19 21:12:54 +0000

    👝🛳 Checkpoint ./commands/diversao/fumar.js:22651380/232 ./watch.json:22651380/6

--- commands/diversao/fumar.js
+++ commands/diversao/fumar.js
@@ -1,2 +1,8 @@
 const Discord = require("discord.js");
-
+//🚬 💨   💨   💨 
+module.exports.run = async (client, message, args) => {
+  message.channel.send("🚬").then(() => {  
+    client.util.delayFor(100)
+    .edit("🚬 💨");
+  })
+}
