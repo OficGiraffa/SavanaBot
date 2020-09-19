a248ab4ede4a199dae0eb66c1a81f157ce5f41f7
Glitch (hello-express) <none>
2020-09-19 21:58:57 +0000

    📖💐 Checkpoint ./commands/diversao/fumar.js:22651380/1790 ./watch.json:22651380/18

--- commands/diversao/fumar.js
+++ commands/diversao/fumar.js
@@ -1,8 +1,12 @@
 module.exports.run = async (client, message, args) => {
   let msg = message.channel.send("🚬").then(async (msg) => {
-    await client.setTimeout(() => {msg.edit("🚬💨")}, 1000).then(async (msg) => {
-      await client.setTimeout(() => {msg.edit("🚬 💨")}, 1000).then(async (msg) => {
-        }
-    }
-  })
+    await client.setTimeout(() => {msg.edit("🚬  💨")}, 1000);
+    await client.setTimeout(() => {msg.edit("🚬      💨")}, 1000);
+    await client.setTimeout(() => {msg.edit("🚬 💨        💨")}, 1000);
+    await client.setTimeout(() => {msg.edit("🚬      💨       💨")}, 1000);
+    await client.setTimeout(() => {msg.edit("🚬           💨")}, 1000);
+    await client.setTimeout(() => {msg.edit("🚬                    💨")}, 1000);
+    await client.setTimeout(() => {msg.edit("🚬                      💨 ")}, 1000);
+    msg.edit("🚬");
+    })
 }
