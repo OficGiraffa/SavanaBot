4977e0e7f262cb1b43359ed3c7205f50581029f2
Glitch (hello-express) <none>
2020-09-19 14:07:07 +0000

    🐚🎷 Checkpoint ./index.js:22651380/611 ./commands/say.js:22651380/135

--- index.js
+++ index.js
@@ -16,7 +16,18 @@ client.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
   if (!message.content.startsWith(config.prefix)) return;
-  if (message.content.startsWith(""))
-})
+  if (message.content.startsWith(`<@!${client.user.id}`) || message.content.startsWith(`<@${client.user.id}`)) return;
+  
+  let args = message.content.split(" ").slice(1);
+  let command = message.content.split(" ")[0];
+  command = command.slice(config.prefix.length);
+  try {
+    let commandFile = require(`./commands/${command}.js`)
+    delete require.cache(require.resolve(`./commands/${command}.js`));
+    return commandFile.run(client, message, args);
+  } catch (err){
+    console.error("Erro: " + err);
+  }
+});
 
 client.login(process.env.TOKEN); //Ligando o Bot caso ele consiga acessar o token
