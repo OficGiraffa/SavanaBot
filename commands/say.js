7450b8641d1cccad41e43921b262b4a3bc07f9db
Glitch (hello-express) <none>
2020-09-19 18:44:21 +0000

    🌗🌇 Checkpoint ./commands/say.js:22651380/95 ./watch.json:22651380/2

--- commands/say.js
+++ commands/say.js
@@ -1,12 +1,13 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-  if (message.member)
-  const sayMessage = args.join(" ");
-  
-  let channelToSend = message.mentions.channels.first() || message.channel; 
-  
-  if (sayMessage !== ""){
-      channelToSend.send(sayMessage.replace(channelToSend, ""));
+  if (message.member.permissions.has("MANAGE_MESSAGES")){
+    const sayMessage = args.join(" ");
+
+    let channelToSend = message.mentions.channels.first() || message.channel; 
+
+    if (sayMessage !== ""){
+        channelToSend.send(sayMessage.replace(channelToSend, ""));
     }
+  }
 }
