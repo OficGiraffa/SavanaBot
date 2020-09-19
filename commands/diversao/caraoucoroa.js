92c1841f1823e6c17646d53803cac95fa4b3464a
Glitch (hello-express) <none>
2020-09-19 20:26:45 +0000

    💡⛱ Checkpoint ./commands/diversao/caraoucoroa.js:22651380/1030 ./watch.json:22651380/16

--- commands/diversao/caraoucoroa.js
+++ commands/diversao/caraoucoroa.js
@@ -1,6 +1,28 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
+module.exports.run = (client, message, args) => {
   let result = Math.floor(Math.random() * 2) + 1;
-  message.channel.send (result);
+  
+  let embed = undefined;
+  
+  if (result === 1){
+    embed = new Discord.MessageEmbed()
+    .setTitle("Cara ou Coroa?")
+    .setColor("YELLOW")
+    .setImage("https://i.colnect.net/f/2919/275/1-Real.jpg")
+    .setDescription("CARA! 😀")
+    .setFooter("Por: " + message.author.username);
+  } else if (result === 2){
+    embed = new Discord.MessageEmbed()
+    .setTitle("Cara ou Coroa?")
+    .setColor("RED")
+    .setImage("https://upload.wikimedia.org/wikipedia/commons/6/6f/Brasil_coin_1_real-2.jpg")
+    .setDescription("COROA! 👑")
+    .setFooter("Por: " + message.author.username);
+  }
+  
+  message.channel.send (embed);
+  
+  
+  
 }
