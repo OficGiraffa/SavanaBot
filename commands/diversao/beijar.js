d4ed34d2776b7258c8d7e19b2e4c97b71da4b399
Glitch (hello-express) <none>
2020-09-20 02:33:23 +0000

    🌍🚓 Checkpoint ./commands/diversao/beijar.js:22651380/168 ./watch.json:22651380/12

--- commands/diversao/beijar.js
+++ commands/diversao/beijar.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-  let kissTo = message.metions.users.first || 
+  let kissTo = message.mentions.users.first() || message.author;
   
   let embed = new Discord.MessageEmbed()
-  .setTitle("Beijo amoroso 😍! De " + )
+  .setTitle("Beijo amoroso 😍! De " + message.author.username + " para " + "@" + kissTo.username)
   .setColor("PINK")
   .setImage("https://media.giphy.com/media/l2Je2M4Nfrit0L7sQ/source.gif")
   .setFooter("Por: " + message.author.username);
