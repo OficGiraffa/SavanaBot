54afd24207886172e892876259ab713acae07e69
Glitch (hello-express) <none>
2020-09-20 11:30:13 +0000

    🎍🎍 Checkpoint ./commands/diversao/beijar.js:22651380/1283 ./watch.json:22651380/2

--- commands/diversao/beijar.js
+++ commands/diversao/beijar.js
@@ -9,9 +9,48 @@ module.exports.run = async (client, message, args) => {
   let embed = new Discord.MessageEmbed()
   .setTitle("Beijo amoroso 😍! De " + message.author.username + " para " + "@" + kissTo.username)
   .setColor("PINK")
-  //.setImage("https://media.giphy.com/media/l2Je2M4Nfrit0L7sQ/source.gif")
+  .setImage(random_gif())
   .setFooter("Por: " + message.author.username);
   
   message.channel.send(embed);
   
+}
+
+function random_gif(){
+   let gif_choise = undefined;
+  
+   var options = {
+        url: "https://giphy.com/search/" + "Kiss",
+        method: "GET",
+        headers: {
+            "Accept": "text/html",
+            "User-Agent": "Giphy"
+        }
+    };
+  
+  Request(options, function(error, response, responseBody) {
+        if (error) {
+            return;
+        }
+ 
+ 
+        $ = Cheerio.load(responseBody);
+ 
+ 
+        var links = $(".image a.link");
+ 
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+       
+        console.log(urls);
+ 
+        if (!urls.length) {
+           
+            return;
+        }
+ 
+        // Send result
+        gif_choise = urls[Math.floor(Math.random() * urls.length)];
+    });
+  
+  return gif_choise;
 }
