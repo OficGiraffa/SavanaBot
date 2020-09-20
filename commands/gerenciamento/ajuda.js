96a24045d4f72c74ead92514e4b242dc5e4ee095
Glitch (hello-express) <none>
2020-09-20 00:16:35 +0000

    🐏😺 Checkpoint ./commands/gerenciamento/ajuda.js:22651380/118 ./watch.json:22651380/6

--- commands/gerenciamento/ajuda.js
+++ commands/gerenciamento/ajuda.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args) => {
   let embed_help_1 = new Discord.MessageEmbed()
   .setTitle("Ajuda do bot!")
   .setColor("RANDOM")
-  .setDescription("Obrigado por adicionar o SavanaBot ao seu servidor!\nFeito por: @Ofic_Giraffa | Comandos:\n")
+  .setDescription("Obrigado por adicionar o SavanaBot ao seu servidor!\nFeito por: @Ofic_Giraffa | PREFIXO: = | Comandos:\n-ajuda  |\n-avatar | -falar |\n -cara_ou_coroa | -comida_soviética | -fumar")
   .setFooter("Por: " + message.author.username);
   
   message.channel.send(embed_help_1);
