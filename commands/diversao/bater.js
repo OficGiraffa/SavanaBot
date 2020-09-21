482462fae5cae9e9e9b0e50d21afeaf94cbf7680
Glitch (hello-express) <none>
2020-09-21 10:32:35 +0000

    🎃🎫 Checkpoint ./commands/diversao/bater.js:22651380/3449

--- commands/diversao/bater.js
+++ commands/diversao/bater.js
@@ -4,51 +4,22 @@ const Request = require("request");
 
 
 module.exports.run = async (client, message, args) => {
-  slap(message);
-}
-
-function slap(message){
-   var options = {
-        url: "https://www.dogpile.com/serp?q=slap+gifs&sc=xG6MSoci07So10",
-        method: "GET",
-        headers: {
-            "Accept": "text/html",
-            "User-Agent": "Chrome"
-        }
-    };
+    let slapTo = message.mentions.users.first() || message.author;
   
-  Request(options, function(error, response, responseBody) {
-        if (error) {
-            return;
-        }
- 
- 
-        $ = Cheerio.load(responseBody);
- 
- 
-        var links = $(".image a.link");
- 
-        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
-       
-        //console.log(urls);
- 
-        if (!urls.length) {
-           
-            return;
-        }
-         
-        let gif_choise = urls[Math.floor(Math.random() * urls.length)];
-    
-        if (gif_choise !== undefined){
-           let slapTo = message.mentions.users.first() || message.author;
-  
-            let embed = new Discord.MessageEmbed()
-            .setTitle("Agora tu tá fudido! " + message.author.username + " bateu em " + "@" + slapTo.username)
-            .setColor("RED")
-            .setImage(gif_choise)
-            .setFooter("Por: " + message.author.username);
+    let embed = new Discord.MessageEmbed()
+    .setTitle("Agora tu tá fudido! " + message.author.username + " bateu em " + "@" + slapTo.username)
+    .setColor("RED")
+    .setImage()
+    .setFooter("Por: " + message.author.username);
   
             message.channel.send(embed);
-        }
-    });
+}
+
+function gif_slap(message){
+  let gifs_url = [
+    
+  ]
+  
+  let total_url = Math.floor(Math.random() * gifs_url.length)
+  console.log(total_url)
 }
