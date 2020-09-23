9f6491e487ffd16f1d0b1f68d31fb3699019ef1e
Glitch (hello-express) <none>
2020-09-23 14:51:57 +0000

    💃🕌 Checkpoint ./commands/utilidades/apagar.js:22651380/481 ./watch.json:22651380/22

--- commands/utilidades/apagar.js
+++ commands/utilidades/apagar.js
@@ -7,8 +7,10 @@ module.exports.run = async (client, message, args) => {
   }
   
   try {
-    message.channel.bulkDelete()
+    message.channel.bulkDelete(msg_clear);
   } finally {
-    message.channel.send(`${msg_clear} mensagens apagadas com sucesso`);
+    message.channel.send(`${msg_clear} mensagens apagadas com sucesso`).then(() => {
+      
+    })
   }
 }
