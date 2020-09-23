cdb49ff830670e1edd03fd4934110f6a05f2a4ee
Glitch (hello-express) <none>
2020-09-23 16:13:07 +0000

    🎣⚓️ Checkpoint ./commands/utilidades/fechar_canal.js:22651380/449 ./watch.json:22651380/8

--- commands/utilidades/fechar_canal.js
+++ commands/utilidades/fechar_canal.js
@@ -1,5 +1,13 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-  let channel_lock = message.mentions.channel
+  let channel_lock = message.mentions.channels.first() || message.channel;
+  
+  try {
+    channel_lock.overwritePermissions([
+      SEND_MESSAGES: false
+    ])
+  } finally {
+    message.channel.send(`<#${channel_lock.id}> fechado com sucesso! 🔥`);
+  }
 }
