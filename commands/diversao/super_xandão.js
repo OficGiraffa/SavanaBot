9d08cf6e257ec5a773326b7f7042ce7ce756d280
Glitch (hello-express) <none>
2020-09-23 14:16:26 +0000

    🎟🏵 Checkpoint ./commands/diversao/super_xandão.js:22651380/21659 ./watch.json:22651380/12

--- commands/diversao/super_xandão.js
+++ commands/diversao/super_xandão.js
@@ -1,7 +1,15 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
+  let msg = args.join(" ");
+  
+  if (!args[0]){
+    return message.reply("Desculpe! Sem ter algo para falar o Super Xandão fica quieto!");
+  }
   message.channel.createWebhook("SUPER XANDÃO", {
-    avatar: ""
+    avatar: "https://pbs.twimg.com/profile_images/1306776294278004736/64yfTxt9_400x400.jpg",
+  }).then((xandao) => {
+    xandao.send(msg);
   })
+  message.delete();
 }
