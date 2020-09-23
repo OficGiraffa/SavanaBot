a5e115fde858f4815041e6cbac852ec5cb4795e6
Glitch (hello-express) <none>
2020-09-23 17:10:11 +0000

    ✂️⛈ Checkpoint ./commands/utilidades/fechar_canal.js:22651380/174 ./watch.json:22651380/8

--- commands/utilidades/fechar_canal.js
+++ commands/utilidades/fechar_canal.js
@@ -4,7 +4,10 @@ module.exports.run = async (client, message, args) => {
   let channel_lock = message.mentions.channels.first() || message.channel;
   
   try {
-    channel_lock.overwritePermissions(message.guild.roles.everyone, {SEND_MESSAGES: false});
+    channel_lock.overwritePermissions([{
+      id: message.guild.roles.everyone.id,
+      deny: ['SEND_MESSAGES']
+    }]);
   } finally {
     message.channel.send(`<#${channel_lock.id}> fechado com sucesso! 🔥`);
   }
