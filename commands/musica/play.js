e3e1871bc743babaa90132339092afa5a210b98e
Glitch (hello-express) <none>
2020-09-24 00:04:36 +0000

    🎢👏 Checkpoint ./commands/musica/play.js:22651380/409 ./watch.json:22651380/12

--- commands/musica/play.js
+++ commands/musica/play.js
@@ -2,5 +2,17 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
   let voice_channel = message.member.voice.channel;
-  message.reply(`Você está no canal: ${voice_channel}`);
+  if (!voice_channel){
+    return message.reply("Desculpe! Você precisa estar em um canal de áudio para escutar áudios! :/ ");
+  }
+  
+  let music_url = 'https://www.youtube.com/watch?v=h5Z-9nZMOx8';
+  
+  let connection = await voice_channel.join();
+  
+  await connection.play(music_url);
+  
+  
+  
+  //message.reply(`Você está no canal: ${voice_channel}`);
 }
