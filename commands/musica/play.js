edb0edc4a33929b85999d920dd3605b6b7553c82
Glitch (hello-express) <none>
2020-09-24 00:51:42 +0000

    🌽🗃 Checkpoint ./commands/musica/play.js:22651380/1024 ./watch.json:22651380/10

--- commands/musica/play.js
+++ commands/musica/play.js
@@ -2,11 +2,20 @@ const Discord = require("discord.js");
 const Ytdl = require("ytdl-core");
 
 module.exports.run = async (client, message, args) => {
+  if (!args[0]){
+    return message.reply("Desculpe! Como vou saber o que você quer escutar se você não falar?");
+  }
+  
   let voice_channel = message.member.voice.channel;
   if (!voice_channel){
     return message.reply("Desculpe! Você precisa estar em um canal de áudio para escutar áudios! :/ ");
   }
-  let connection = await voice_channel.join();
   
-  let dispatcher = await connection.play(Ytdl("https://www.youtube.com/watch?v=GNRjFAxpyRI"));
+  if (client.voice.channel){
+    return message.reply("Desculpe! Já estou sendo usado em outro canal!");
+  } else {
+    let connection = await voice_channel.join();
+  
+    let dispatcher = await connection.play(Ytdl(args[0], { filter: 'audioonly' })); 
+  }
 }
