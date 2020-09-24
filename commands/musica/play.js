7bfb0f7f9c3a3bf13bc3eee9e43347c608fdf8a4
Glitch (hello-express) <none>
2020-09-24 11:01:29 +0000

    🎱🍈 Checkpoint ./watch.json:22651380/454 ./commands/musica/play.js:22651380/183 ./index.js:22651380/7438 ./package.json:22651380/2701

--- commands/musica/play.js
+++ commands/musica/play.js
@@ -1,12 +1,13 @@
 const Discord = require("discord.js");
 const Ytdl = require("ytdl-core");
-
-module.exports.run = async (client, message, args) => {
+//"@discordjs/opus": "^0.3.2"
+exports.run = async (client, message, args) => {
   if (!args[0]){
     return message.reply("Desculpe! Como vou saber o que você quer escutar se você não falar?");
   }
   
   let voice_channel = message.member.voice.channel;
+  
   if (!voice_channel){
     return message.reply("Desculpe! Você precisa estar em um canal de áudio para escutar áudios! :/ ");
   }
@@ -23,7 +24,7 @@ module.exports.run = async (client, message, args) => {
   
   let embed_play = new Discord.MessageEmbed()
   .setTitle("Estou tocando um áudio!")
-  .setDescription()
+  .setDescription(music_url.info)
   .setColor("RANDOM")
   .setFooter(`Por ${message.author.username}`);
   
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.get("/", (request, response) => {
   const ping = new Date();
   ping.setHours(ping.getHours() - 3);
-  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
+  console.log(
+    `Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`
+  );
   response.sendStatus(200);
 });
 app.listen(process.env.PORT); // Recebe solicitações que o deixa online
@@ -23,89 +25,100 @@ client.on("ready", async message => {
   setInterval(() => {
     client.user.setActivity(status[Math.floor(Math.random() * status.length)]);
   }, 10000);
-  
-  let servers = client.guilds.cache.size
-  
+
+  let servers = client.guilds.cache.size;
+
   let embed_status = new Discord.MessageEmbed()
-  .setTitle("Status: Ligado ou reiniciado")
-  .setDescription(`Sou o SavanaBot! Estou em ${servers} servers e estou pronto para uso!\n` + 
-                  `Neste server tem ${client.guilds.cache.get("738458523775533177").memberCount} membros!`)
-  .setColor("GREEN")
-  .setFooter("SavanaBot_Status");
-  
-  client.guilds.cache.get("738458523775533177").channels.cache.get("758101838116683826").send(embed_status);
-  
-  client.guilds.cache.get("738458523775533177").channels.cache.get("739847372116459592").setTopic("Membros: " + client.guilds.cache.get("738458523775533177").memberCount);
-})
+    .setTitle("Status: Ligado ou reiniciado")
+    .setDescription(
+      `Sou o SavanaBot! Estou em ${servers} servers e estou pronto para uso!\n` +
+        `Neste server tem ${
+          client.guilds.cache.get("738458523775533177").memberCount
+        } membros!`
+    )
+    .setColor("GREEN")
+    .setFooter("SavanaBot_Status");
+
+  client.guilds.cache
+    .get("738458523775533177")
+    .channels.cache.get("758101838116683826")
+    .send(embed_status);
+
+  client.guilds.cache
+    .get("738458523775533177")
+    .channels.cache.get("739847372116459592")
+    .setTopic(
+      "Membros: " + client.guilds.cache.get("738458523775533177").memberCount
+    );
+});
 
 client.on("guildMemberAdd", async member => {
-  client.guilds.cache.get("738458523775533177").channels.cache.get("739847372116459592").setTopic("Membros: " + client.guilds.cache.get("738458523775533177").memberCount);
-})
+  client.guilds.cache
+    .get("738458523775533177")
+    .channels.cache.get("739847372116459592")
+    .setTopic(
+      "Membros: " + client.guilds.cache.get("738458523775533177").memberCount
+    );
+});
 client.on("guildMemberRemove", async member => {
-  client.guilds.cache.get("738458523775533177").channels.cache.get("739847372116459592").setTopic("Membros: " + client.guilds.cache.get("738458523775533177").memberCount);
-})
+  client.guilds.cache
+    .get("738458523775533177")
+    .channels.cache.get("739847372116459592")
+    .setTopic(
+      "Membros: " + client.guilds.cache.get("738458523775533177").memberCount
+    );
+});
 
 client.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
   if (!message.content.startsWith(config.prefix)) return;
-  if (message.content.startsWith(`<@!${client.user.id}`) || message.content.startsWith(`<@${client.user.id}`)) return;
-  
+  if (
+    message.content.startsWith(`<@!${client.user.id}`) ||
+    message.content.startsWith(`<@${client.user.id}`)
+  )
+    return;
+
   let prefixes = JSON.parse(Fs.readFileSync("./prefixes.json", "utf8"));
-  if (!prefixes[message.guild.id]){
+  if (!prefixes[message.guild.id]) {
     prefixes[message.guild.id] = {
       prefixes: config.prefix
-    }
+    };
   }
-  
+
   let prefix = prefixes[message.guild.id].prefixes;
   let args = message.content.split(" ").slice(1);
   let command = message.content.split(" ")[0];
   command = command.slice(prefix.length);
   command = command.toLowerCase();
-  
+
   try {
     let commandFile = require(`./commands/utilidades/${command}.js`);
-    
+
     //delete require.cache(require.resolve(`./commands/${command}.js`));
-    
+
     return commandFile.run(client, message, args);
-    
   } catch {
-    
     try {
-      
       let commandFile = require(`./commands/diversao/${command}.js`);
-      
+
       return commandFile.run(client, message, args);
-      
     } catch {
-      
       try {
-      
-      let commandFile = require(`./commands/gerenciamento/${command}.js`);
-      
-      return commandFile.run(client, message, args);
-      
-    } catch {
-      
-      try {
-      
-      let commandFile = require(`./commands/musica/${command}.js`);
-      
-      return commandFile.run(client, message, args);
-        
-    } catch (err) {
-      
-      console.log(err);
-      
+        let commandFile = require(`./commands/gerenciamento/${command}.js`);
+
+        return commandFile.run(client, message, args);
+      } catch {
+        try {
+          let commandFile = require(`./commands/musica/${command}.js`);
+
+          return commandFile.run(client, message, args);
+        } catch (err) {
+          console.log(err);
+        }
+      }
     }
-  }
-  }
   }
 });
 
-
-
 client.login(process.env.TOKEN); //Ligando o Bot caso ele consiga acessar o token
-
