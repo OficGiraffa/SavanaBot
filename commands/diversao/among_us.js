8318dc0ba65980517bc047829804c79653e4d9c6
Glitch (hello-express) <none>
2020-09-24 19:03:07 +0000

    🍦🌓 Checkpoint ./commands/diversao/among_us.js:22651380/421 ./watch.json:22651380/8

--- commands/diversao/among_us.js
+++ commands/diversao/among_us.js
@@ -2,17 +2,10 @@ const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
   
-  let members = []
   let member;
-  message.guild.members.forEach(member => {
-    members.push(member);
-    if (members.lenght > 100){
-      return;
-    }
-  }).then(async () => {
-    let i = Math.floor(Math.random() * members.lenght);
-    member = members[i];
-  });
+  let members = message.guild.members.filter(member => member.presence.status === "online");
+  
+  console.log(member);
   
   console.log(member);  
   
@@ -23,7 +16,7 @@ exports.run = async (client, message, args) => {
   message.channel.send(". 　　　。　　　　•　 　ﾟ　　。 　　. \n" +
                        " 　　　.　　　 　　.　　　　　。　　 。　.\n" +　
                        ".　　 。　　　　　 ඞ 。 . 　　 • 　　　　•\n" +
-                       `　ﾟ　　 ${perso} não era um impostor　 。　. \n` +
+                       `　ﾟ　　 ${member} não era um impostor　 。　. \n` +
                        ` '　　　 ${restantes} Restantes impostores 　 　　。\n` +
                         　"　ﾟ　　　.　　　. ,　　　　.　 .")
 }
