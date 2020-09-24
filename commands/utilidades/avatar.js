682e7fc6e906bff68457dc68d2586947daa05a5c
Glitch (hello-express) <none>
2020-09-24 21:44:47 +0000

    📍🐺 Checkpoint ./commands/utilidades/apagar.js:22651380/1377 ./commands/utilidades/avatar.js:22651380/1319

--- commands/utilidades/avatar.js
+++ commands/utilidades/avatar.js
@@ -1,19 +1,27 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-  
-  let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
-  
-  let avatar = user.displayAvatarURL({ dynamic: true, format: 'png', size: 1024 });
-  
+  let user =
+    message.mentions.users.first() ||
+    client.users.cache.get(args[0]) ||
+    message.author;
+
+  let avatar = user.displayAvatarURL({
+    dynamic: true,
+    format: "png",
+    size: 1024
+  });
+
   let embed = new Discord.MessageEmbed()
-  .setTitle(`🖼 Avatar de ${user.username}`)
-  .setDescription(`[clique aqui](${avatar}) para baixar a foto.\n\npatin ajudou tmbm ;)`)
-  .setColor("RANDOM")
-  .setImage(avatar)
-  .setFooter("Por: " + message.author.username);
-  
+    .setTitle(`🖼 Avatar de ${user.username}`)
+    .setDescription(
+      `[clique aqui](${avatar}) para baixar a foto.\n\npatin ajudou tmbm ;)`
+    )
+    .setColor("RANDOM")
+    .setImage(avatar)
+    .setFooter("Por: " + message.author.username);
+
   console.log(message.author.displayAvatarURL);
-  
+
   message.channel.send(embed);
-}
+};
