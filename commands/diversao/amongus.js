e0ec8da2309728e7540054ec13f94c1398a4c188
Glitch (hello-express) <none>
2020-09-24 15:29:38 +0000

    ⛪️🙌 Checkpoint ./commands/diversao/amongus.js:22651380/363

---
+++ commands/diversao/amongus.js
@@ -0,0 +1,12 @@
+const Discord = require("discord");
+
+//. 　　　。　　　　•　 　ﾟ　　。 　　.
+//　　　.　　　 　　.　　　　　。　　 。　. 　
+//.　　 。　　　　　 ඞ 。 . 　　 • 　　　　•
+//　　ﾟ　　 @Gab  ele não era um impostor　 。　.
+//　　'　　　 1 Restantes impostores 　 　　。
+//　　ﾟ　　　.　　　. ,　　　　.　 .
+
+module.exports.run = async (client, message, args) => {
+  let num_impostor = new Discord.Collector();
+}
