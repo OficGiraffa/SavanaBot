6e8aaf9540f087addd1d4c767127a190046f1320
Glitch (hello-express) <none>
2020-09-25 01:28:48 +0000

    🚿🌼 Checkpoint ./commands/diversao/among_us.js:22651380/131 ./watch.json:22651380/4

--- commands/diversao/among_us.js
+++ commands/diversao/among_us.js
@@ -1,20 +1,20 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-  
-  let member;
+
   let members = message.mentions.users;
   
-  console.log(members);
+  console.log(members[])
   
   let restantes = 0;
   
   
-  
+  /*
   message.channel.send(". 　　　。　　　　•　 　ﾟ　　。 　　. \n" +
                        " 　　　.　　　 　　.　　　　　。　　 。　.\n" +　
                        ".　　 。　　　　　 ඞ 。 . 　　 • 　　　　•\n" +
                        `　ﾟ　　 ${member} não era um impostor　 。　. \n` +
                        ` '　　　 ${restantes} Restantes impostores 　 　　。\n` +
                         　"　ﾟ　　　.　　　. ,　　　　.　 .")
+                         */
 }
