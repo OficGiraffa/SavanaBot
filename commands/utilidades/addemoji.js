d97fea7004c4cc7d4f0b9f8023bbfbb23cf16f43
Glitch (hello-express) <none>
2020-09-29 20:31:11 +0000

    🍠📖 Checkpoint ./commands/utilidades/addemoji.js:22651380/587

--- commands/utilidades/addemoji.js
+++ commands/utilidades/addemoji.js
@@ -1,7 +1,15 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (message, client, args) => {
+  //=addemoji
+  //Envia: qual emoji você quer adicionar? => coletor msg
+  //Guarda numa variavel
+  //Entra na guild da message
+  //Adiciona o emoji com GUILD.emojis.create(var do emoji)
   
+  if (!message.member.permissions.has("MANAGE_EMOJIS")){
+    return 
+  }
   
   
 }
