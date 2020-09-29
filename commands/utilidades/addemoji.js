983d9f62da3c68ee7d515f270297e8558cd54ee3
Glitch (hello-express) <none>
2020-09-29 20:43:09 +0000

    👮🎑 Checkpoint ./commands/utilidades/addemoji.js:22651380/1138

--- commands/utilidades/addemoji.js
+++ commands/utilidades/addemoji.js
@@ -1,15 +1,25 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (message, client, args) => {
-  //=addemoji
-  //Envia: qual emoji você quer adicionar? => coletor msg
   //Guarda numa variavel
   //Entra na guild da message
   //Adiciona o emoji com GUILD.emojis.create(var do emoji)
   
   if (!message.member.permissions.has("MANAGE_EMOJIS")){
-    return 
-  }
+    return message.reply("Desculpe, você não tem permissão pra mexer nos emojis! >:c");
+  };
   
+  if (!args[0]){
+    return message.reply("Desculpe, você não citou nenhum emoji! (Ou quer que eu escolha pra você? >:c)");
+  };
+  
+  let emoji_to_add = args[0];
+  
+  try {
+  message.guild.emojis.create(emoji_to_add);
+  
+  message.reply("Yee! Emoji adicionado com sucesso!");
+  
+  }
   
 }
